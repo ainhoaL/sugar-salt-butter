428270bb0df26631debe7ba4208fcf1d41dfc9a5
nhoa <ainhoa.larumbe@gmail.com>
2019-06-03 17:32:09 +0100

    fix routes

--- routes.js
+++ routes.js
@@ -4,9 +4,9 @@ const RecipesController = require('./controllers/recipes_controller')
 
 router.post('/api/v1/recipes', (req, res) => RecipesController.create(req, res))
 
-router.get('/api/v1/recipes/:id', (req, res) => RecipesController.getRecipe(req, res))
-
 // TODO: maybe move on to GraphQL once we need to do more searches and filtering ?
 router.get('/api/v1/recipes/search', (req, res) => RecipesController.find(req, res))
+
+router.get('/api/v1/recipes/:id', (req, res) => RecipesController.getRecipe(req, res))
 
 module.exports = router
